b39b9c6c514962b7e224188440e21ce564135531
Nataliss <oksklient@gmail.com>
2023-02-02 20:38:31 +0000

    Add hw part 2

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,12 +1,14 @@
 import { galleryItems } from "./gallery-items.js";
 // Change code below this line
+console.log(basicLightbox)
+// import * as basicLightbox from "basiclightbox";
 
-const divContainer = document.querySelector("div");
+const divContainer = document.querySelector(".gallery");
 
-const imagesMarkup = createImagesMarkap(galleryItems);
+const imagesMarkup = createImagesMarkup(galleryItems);
 divContainer.insertAdjacentHTML("beforeend", imagesMarkup);
 
-function createImagesMarkap(galleryItems) {
+function createImagesMarkup(galleryItems) {
   const markup = galleryItems
     .map(({ preview, original, description }) => {
       return `<div class="gallery__item">
@@ -21,8 +23,31 @@ function createImagesMarkap(galleryItems) {
 </div>`;
     })
     .join("");
+  return markup;
 }
 
-console.log(createImagesMarkap);
+divContainer.addEventListener("click", onClickOpenModal);
+// divContainer.addEventListener("click", (event) => {
+//   if (event.target === event.currentTarget) {
+//     closeImage();
+//   }
+// });
+function onClickOpenModal(event) {
+  event.preventDefault();
 
-console.log(galleryItems);
+  if (event.target.nodeName !==  "IMG") {
+    return;
+  }
+
+  const instance = basicLightbox.create(
+    `<img src="${event.target.dataset.source}" width="800" height = "600"> `
+  );
+  instance.show();
+
+  divContainer.addEventListener("keydown", closeModalEsc);
+  function closeModalEsc(event) {
+    if (event.code === "Escspe") {
+      instance.close();
+    }
+  }
+}
--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,46 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
+const gallery = document.querySelector(".gallery");
+const items = [];
+// const imagesMarkup = createImagesMarkup(galleryItems);
+// gallery.insertAdjacentHTML("beforeend", imagesMarkup);
+
+// function createImagesMarkup(galleryItems) {
+//   const markup = galleryItems
+//     .map(({ preview, original, description }) => {
+//       return `<div class="gallery__item">
+//   <a class="gallery__item" href="${original}">
+//     <img
+//       class="gallery__image"
+//       src="${preview}"
+//       alt="${description}"
+//     />
+//   </a>
+// </div>`;
+//     })
+//     .join("");
+//   return markup;
+// }
+
+galleryItems.forEach((element) => {
+  const galleryLink = document.createElement("a");
+  galleryLink.className = "gallery__link";
+  galleryLink.href = element.original;
+
+  const galleryImage = document.createElement("img");
+  galleryImage.className = "gallery__image";
+  galleryImage.src = element.preview;
+  galleryImage.setAttribute("title", element.description);
+  galleryImage.alt = element.description;
+
+  galleryLink.append(galleryImage);
+  items.push(galleryLink);
+});
+gallery.append(...items);
+
+new SimpleLightbox(".gallery a", {
+  captionDelay: 250,
+});
+
+
